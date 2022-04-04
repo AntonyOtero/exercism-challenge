a774a586fcfd5df00062257895c197ddef31f593
= <=>
2022-04-04 09:36:57 -0400

    Remove unused useState import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Button from "./Button"
 
 export default function Footer({ current_page, total_count, total_pages, handlePageChange }) {
