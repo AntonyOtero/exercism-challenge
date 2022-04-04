66433b53697fa50da1489721a3b47a6a05a229a9
= <=>
2022-04-04 08:21:05 -0400

    Fix Total Count so that it doesn't update when data is filtered by track

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true)
   const [trackCounts, setTrackCounts] = useState({})
   const [results, setResults] = useState([])
+  const [totalCount, setTotalCount] = useState(0)
   const [pagination, setPagination] = useState({})
   const [currentPage, setCurrentPage] = useState(1)
   const [trackFilter, setTrackFilter] = useState("")
@@ -38,19 +39,20 @@ function App() {
         break
       }
   }
-  console.log(Object.keys(pagination).length)
+  console.log(pagination)
   useEffect(() => {
     if (!isLoading) {
       setIsLoading(true)
     }
       fetch(`https://exercism.org/api/v2/hiring/testimonials?page=${currentPage}${trackFilter}${sortFilter}`)
       .then(res => res.json())
-        .then(data => {
+      .then(data => {
         setTrackCounts(data.testimonials.track_counts)
         setResults(data.testimonials.results)
-        if (Object.keys(pagination).length === 0) {
-          setPagination(data.testimonials.pagination)
+        if (totalCount === 0) {
+          setTotalCount(data.testimonials.pagination.total_count)
         }
+        setPagination(data.testimonials.pagination)
         setIsLoading(false)
       })
     }, [currentPage, sortFilter, trackFilter])
@@ -62,14 +64,14 @@ function App() {
           <TestimonialsIcon />
           <div className='flex items-center mt-3 mb-4'>
             <h1 className='mx-4 font-bold text-3xl text-[#130B43]'>Testimonials I've Left</h1>
-            <Tag label={pagination.total_count} />
+            <Tag label={totalCount} />
           </div>
           <HeaderDivider />
         </header>
         <div className='relative mx-auto pt-20 pb-[70px] mb-10 w-[1376px] h-[791px] rounded-lg shadow-[0_4px_42px_rgba(79,114,205,0.15)]'>
           <Header
             trackCounts={trackCounts}
-            totalCount={pagination.total_count}
+            totalCount={totalCount}
             handleSorting={handleSorting}
             handleTrackFilter={handleTrackFilter}
           />
