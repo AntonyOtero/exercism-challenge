be75046fca732133c840f55a987034a23e73670b
= <=>
2022-04-07 17:13:34 -0400

    Update testing libraries

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/outline"
 
 export default function Button({type, label, style, currentPage,  handlePageChange}) {
   return (
-    <button className={`min-w-[37px] px-4 py-2 mx-1.5 text-sm font-medium border rounded-md border-slate-200 ${currentPage === label ? "text-theme-default bg-theme-faded-100 border-theme-faded-200 pointer-events-none" : "text-theme-faded-200 bg-white border-theme-light-500"} ${style}`} onClick={handlePageChange}>
+    <button type="button" className={`min-w-[37px] px-4 py-2 mx-1.5 text-sm font-medium border rounded-md border-slate-200 ${currentPage === label ? "text-theme-default bg-theme-faded-100 border-theme-faded-200 pointer-events-none" : "text-theme-faded-200 bg-white border-theme-light-500"} ${style}`} onClick={handlePageChange}>
       { type === "Prev" && <ArrowLeftIcon className="h-3 mr-2.5 pointer-events-none"/> }
       {label}
       { type === "Next" && <ArrowRightIcon className="h-3 ml-2.5 pointer-events-none"/> }
--- src/components/SortFilter.jsx
+++ src/components/SortFilter.jsx
@@ -18,55 +18,53 @@ export default function Sort({handleSorting }) {
   return (
     <Listbox value={selectedSort} onChange={setSelectedSort}>
       {({ open }) => (
-        <>
-          <div className="w-80 relative">
-            <Listbox.Button className="relative w-full bg-theme-light-200 border border-transparent rounded-md shadow-sm cursor-pointer focus:outline-none focus:text-theme-default focus:bg-white focus:border focus:border-theme-active focus:shadow-theme-glow sm:text-sm">
-              <span className="flex items-center">
-                <span className="block my-3 mx-5 text-base text-theme-faded-300 truncate">
-                  Sort by {selectedSort}
-                </span>
+        <div className="w-80 relative">
+          <Listbox.Button className="relative w-full bg-theme-light-200 border border-transparent rounded-md shadow-sm cursor-pointer focus:outline-none focus:text-theme-default focus:bg-white focus:border focus:border-theme-active focus:shadow-theme-glow sm:text-sm">
+            <span className="flex items-center">
+              <span className="block my-3 mx-5 text-base text-theme-faded-300 truncate">
+                Sort by {selectedSort}
               </span>
-              <span className="ml-1.5 absolute inset-y-0 right-5 flex items-center pr-2 pointer-events-none">
-                <ChevronDownIcon className="w-6 stroke-theme-faded-300" aria-hidden="true" />
-              </span>
-            </Listbox.Button>
+            </span>
+            <span className="ml-1.5 absolute inset-y-0 right-5 flex items-center pr-2 pointer-events-none">
+              <ChevronDownIcon className="w-6 stroke-theme-faded-300" aria-hidden="true" />
+            </span>
+          </Listbox.Button>
 
-            <Transition
-              show={open}
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                {options.map((option, index) => (
-                  <Listbox.Option
-                    key={index}
-                    className={({ active }) =>
-                      classNames(
-                        active ? 'text-theme-faded-400 bg-theme-light-200' : 'text-theme-faded-400',
-                        'cursor-default select-none relative py-2 pl-3 pr-9'
-                      )
-                    }
-                    value={option}
-                  >
-                    {({ selected }) => (
-                      <>
-                        <div className="flex items-center">
-                          <span
-                            className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3')}
-                          >
-                            {option}
-                          </span>
-                        </div>
-                      </>
-                    )}
-                  </Listbox.Option>
-                ))}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </>
+          <Transition
+            show={open}
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
+              {options.map((option, index) => (
+                <Listbox.Option
+                  key={index}
+                  className={({ active }) =>
+                    classNames(
+                      active ? 'text-theme-faded-400 bg-theme-light-200' : 'text-theme-faded-400',
+                      'cursor-default select-none relative py-2 pl-3 pr-9'
+                    )
+                  }
+                  value={option}
+                >
+                  {({ selected }) => (
+                    <>
+                      <div className="flex items-center">
+                        <span
+                          className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3')}
+                        >
+                          {option}
+                        </span>
+                      </div>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
       )}
     </Listbox>
   )
